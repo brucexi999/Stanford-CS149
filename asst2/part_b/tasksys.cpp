4d5facb46d205243c9d98824416750b68862ef39
Bruce Xi <91307696+brucexi999@users.noreply.github.com>
2025-05-26 20:53:01 -0400

    assignment 2 part b done

--- asst2/part_b/tasksys.cpp
+++ asst2/part_b/tasksys.cpp
@@ -1,9 +1,10 @@
+#include <thread>
+#include <iostream>
 #include "tasksys.h"
-
 
 IRunnable::~IRunnable() {}
 
-ITaskSystem::ITaskSystem(int num_threads) {}
+ITaskSystem::ITaskSystem(int num_threads) : num_threads(num_threads) {}
 ITaskSystem::~ITaskSystem() {}
 
 /*
@@ -133,6 +134,86 @@ TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(int n
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+    threads.reserve(num_threads);
+    // Book keeper thread
+    threads.emplace_back(
+        [this]() {
+            // For each entry in waiting_launches, constantly check its num_dependency, if it's 0, delete it from waiting_launches and 
+            // num_dependency, enqueue all of its tasks to the task queue.
+            while (true) {
+                std::unique_lock<std::mutex> lock(wait_queue_mutex);
+                condition.wait(lock, [this]() {return sync_flag || !running;});
+                if (!running && launch_counter==0) {
+                    return;
+                }
+                for (auto it = waiting_launches.begin(); it != waiting_launches.end();) {
+                    const TaskID& launch_id = it->first;
+                    const BulkLaunch& launch = it->second;
+                    if (num_dependency[launch_id] == 0) {
+                        task_counters[launch_id] = 0;
+                        for (int i = 0; i < launch.num_total_tasks; i++) {
+                            task_queue.push(
+                                {launch_id, 
+                                [=]() { launch.runnable->runTask(i, launch.num_total_tasks); }}
+                            );
+                            task_counters[launch_id]++;
+                        }
+                        condition.notify_all();  // wake up the worker thread
+                        it = waiting_launches.erase(it);  // Safely erase and update iterator
+                        num_dependency.erase(launch_id);
+                    } else {
+                        it++;  // Move to next element only if not erased
+                    }
+                }
+                // If no more ready launch in the waiting queue, go to sleep, it will be awaken when a launch is done.
+                if (launch_counter==0){
+                    all_launches_done = true;
+                    condition.notify_all();
+                    return;
+                }
+
+            }
+        }
+    );
+
+    for (int i = 1; i < num_threads; i++) {
+        threads.emplace_back(
+            [this]() {
+                while(true) {
+                    std::pair<TaskID, std::function<void()>> pair;
+                    std::function<void()> task;
+                    TaskID launch_id; 
+                    {
+                        std::unique_lock<std::mutex> lock (queue_mutex);
+                        condition.wait(lock, [this]() {return (sync_flag && !task_queue.empty()) || !running;});
+                        if (!running && task_queue.empty()) {
+                            return;
+                        }
+                        pair = std::move(task_queue.front());
+                        task_queue.pop();
+                    }
+                    launch_id = pair.first;
+                    task = pair.second;
+                    task();
+                    {
+                        std::unique_lock<std::mutex> lock (queue_mutex);
+                        task_counters[launch_id] --;
+                        // A launch is done, decrease the dependency count for all of its dependents, notify the book keeper thread so that it can enqueue new tasks
+                        if (task_counters[launch_id] == 0) {
+
+                            launch_counter--;
+                            for(auto& dependent_launch_id : dependents[launch_id]) {
+                                num_dependency[dependent_launch_id] --;
+                            }
+                            dependents.erase(launch_id);
+                            condition.notify_all();  // Awake the book keeper thread
+                        }
+                    }
+                }
+            }
+        );
+    }
+
 }
 
 TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
@@ -142,6 +223,19 @@ TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+    {
+        std::unique_lock<std::mutex> lock (queue_mutex);
+        running = false;
+        condition.notify_all();
+        // Clear containers to ensure no stale data
+    }
+    condition.notify_all(); // Second notification to catch stragglers
+    
+    for (auto& thread: threads) {  // auto& thread: thread is a reference (&) to each std::thread object in threads, allowing modification (e.g., calling join()).
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
 }
 
 void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
@@ -165,12 +259,23 @@ TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable* runnabl
     //
     // TODO: CS149 students will implement this method in Part B.
     //
+    const TaskID launch_id = next_launch_id;
+    next_launch_id++;
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        // Store the current bulk launch in waiting_launches
+        waiting_launches[launch_id] = {runnable, num_total_tasks, deps};
+        launch_counter++;
+        // Initialize dependency count
+        num_dependency[launch_id] = deps.size();
 
-    for (int i = 0; i < num_total_tasks; i++) {
-        runnable->runTask(i, num_total_tasks);
+        // QUESTION Seems like, for every previous launch in deps, add the current launch as a dependent
+        for (TaskID dep_id : deps) {
+            dependents[dep_id].insert(launch_id);
+        }
+        condition.notify_all(); // Wake up the book keeper thread
     }
-
-    return 0;
+    return launch_id;
 }
 
 void TaskSystemParallelThreadPoolSleeping::sync() {
@@ -178,6 +283,10 @@ void TaskSystemParallelThreadPoolSleeping::sync() {
     //
     // TODO: CS149 students will modify the implementation of this method in Part B.
     //
-
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    sync_flag = true;
+    condition.notify_all();
+    condition.wait(lock, [this]() { return all_launches_done; });
+    
     return;
 }
--- asst2/part_b/tasksys.h
+++ asst2/part_b/tasksys.h
@@ -1,6 +1,14 @@
 #ifndef _TASKSYS_H
 #define _TASKSYS_H
 
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <atomic>
+#include <unordered_map>
+#include <set>
 #include "itasksys.h"
 
 /*
@@ -68,6 +76,37 @@ class TaskSystemParallelThreadPoolSleeping: public ITaskSystem {
         TaskID runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
                                 const std::vector<TaskID>& deps);
         void sync();
+
+    private:
+        // Vector to store worker threads
+        std::vector<std::thread> threads;
+        // Queue of tasks
+        std::queue<std::pair<TaskID, std::function<void()>>> task_queue;
+        // Mutex to synchronize access to shared data
+        std::mutex queue_mutex;
+        std::mutex wait_queue_mutex;
+        // Condition variable to signal changes in the state of the task queue
+        std::condition_variable condition;
+        // Signals threads to exit
+        // std::atomic<bool> running{true};
+        bool running = true;
+
+        struct BulkLaunch
+        {
+            IRunnable* runnable;
+            int num_total_tasks;
+            std::vector<TaskID> deps;
+        };
+
+        // TaskID is actually the ID for each bulk task launch, not the IDs for individual tasks within 1 launch
+        TaskID next_launch_id = 0;  // Start the ID at 1, it is said 0 can cause problem
+        std::unordered_map<TaskID, BulkLaunch> waiting_launches; // The launches that are not ready to run due to dependencies
+        std::unordered_map<TaskID, std::set<TaskID>> dependents;  // The dependencies (a set of TaskIDs of previous launches) of each launch (indexed by its TaskID), QUESTION why the set data structure? Why not just store the vector?
+        std::unordered_map<TaskID, int> num_dependency;  // The remaining dependency count for a launch
+        std::unordered_map<TaskID, int> task_counters;
+        unsigned int launch_counter = 0;
+        bool sync_flag = false;
+        bool all_launches_done = false;
 };
 
 #endif
